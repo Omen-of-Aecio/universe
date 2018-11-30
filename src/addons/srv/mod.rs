97105ddb1161112ca04a5cbfcd65562b5877f2bb
Kevin Robert Stravers <macocio@gmail.com>
2018-12-01 00:31:53 +0100

    Move srv out of impls

--- src/addons/srv/mod.rs
+++ src/addons/srv/mod.rs
@@ -69,105 +69,103 @@ pub fn create_server(config: &Config) -> Server {
     }
 }
 
-impl Server {
-    fn handle_message(&mut self, src: SocketAddr, msg: &Message) -> Result<(), Error> {
-        // TODO a lot of potential for abstraction/simplification...
+fn handle_message(s: &mut Server, src: SocketAddr, msg: &Message) -> Result<(), Error> {
+    // TODO a lot of potential for abstraction/simplification...
 
-        // Will ignore packets from unregistered connections
-        match *msg {
-            Message::Join { snapshot_rate } => self.new_connection(src, snapshot_rate)?,
-            Message::Input(input) => {
-                let con = self
-                    .connections
-                    .get(&src)
-                    .ok_or_else(|| format_err!("SocketAddr not registererd as player"))?;
-                self.game.input(con.ecs_id, input)?;
-            }
-            Message::ToggleGravity => self.game.toggle_gravity(),
-            Message::BulletFire { direction } => {
-                let con = self
-                    .connections
-                    .get(&src)
-                    .ok_or_else(|| format_err!("SocketAddr not registererd as player"))?;
-                self.game.bullet_fire(con.ecs_id, direction)?;
-            }
-            _ => {}
+    // Will ignore packets from unregistered connections
+    match *msg {
+        Message::Join { snapshot_rate } => new_connection(s, src, snapshot_rate)?,
+        Message::Input(input) => {
+            let con = s
+                .connections
+                .get(&src)
+                .ok_or_else(|| format_err!("SocketAddr not registererd as player"))?;
+            s.game.input(con.ecs_id, input)?;
         }
-        Ok(())
+        Message::ToggleGravity => s.game.toggle_gravity(),
+        Message::BulletFire { direction } => {
+            let con = s
+                .connections
+                .get(&src)
+                .ok_or_else(|| format_err!("SocketAddr not registererd as player"))?;
+            s.game.bullet_fire(con.ecs_id, direction)?;
+        }
+        _ => {}
     }
+    Ok(())
+}
 
-    fn new_connection(&mut self, src: SocketAddr, snapshot_rate: f32) -> Result<(), Error> {
-        info!("New connection!");
-        // Add new player
-        let (w_count, b_count) = self.game.count_player_colors();
-        let color = if w_count >= b_count {
-            Color::Black
-        } else {
-            Color::White
-        };
-        let player_id = self.game.add_player(color);
-        let _ = self
-            .connections
-            .insert(src, Connection::new(player_id, snapshot_rate));
+fn new_connection(s: &mut Server, src: SocketAddr, snapshot_rate: f32) -> Result<(), Error> {
+    info!("New connection!");
+    // Add new player
+    let (w_count, b_count) = s.game.count_player_colors();
+    let color = if w_count >= b_count {
+        Color::Black
+    } else {
+        Color::White
+    };
+    let player_id = s.game.add_player(color);
+    let _ = s
+        .connections
+        .insert(src, Connection::new(player_id, snapshot_rate));
 
-        // Tell about the game size and other meta data
-        self.socket.send_to(
-            Message::Welcome {
-                width: self.game.get_width() as u32,
-                height: self.game.get_height() as u32,
-                you: player_id,
-                white_base: self.game.white_base,
-                black_base: self.game.black_base,
-            },
-            src,
-        )?;
+    // Tell about the game size and other meta data
+    s.socket.send_to(
+        Message::Welcome {
+            width: s.game.get_width() as u32,
+            height: s.game.get_height() as u32,
+            you: player_id,
+            white_base: s.game.white_base,
+            black_base: s.game.black_base,
+        },
+        src,
+    )?;
 
-        // Send it the whole world
-        // We will need to split it up because of limited package size
-        let (packet_w, packet_h) = Server::packet_dim(Socket::max_payload_size() as usize);
-        let blocks = (
-            self.game.get_width() / packet_w + 1,
-            self.game.get_height() / packet_h + 1,
-        );
-        info!("blocks {:?}", blocks);
-        for x in 0..blocks.0 {
-            for y in 0..blocks.1 {
-                // info!("world packet {},{}, {},{}", x * packet_w, y * packet_h, packet_w, packet_h);
-                let msg = self.wrap_game_rect(x * packet_w, y * packet_h, packet_w, packet_h);
-                if let Some(msg) = msg {
-                    self.socket.send_reliably_to(msg, src, None)?;
-                }
+    // Send it the whole world
+    // We will need to split it up because of limited package size
+    let (packet_w, packet_h) = packet_dim(Socket::max_payload_size() as usize);
+    let blocks = (
+        s.game.get_width() / packet_w + 1,
+        s.game.get_height() / packet_h + 1,
+    );
+    info!("blocks {:?}", blocks);
+    for x in 0..blocks.0 {
+        for y in 0..blocks.1 {
+            // info!("world packet {},{}, {},{}", x * packet_w, y * packet_h, packet_w, packet_h);
+            let msg = wrap_game_rect(s, x * packet_w, y * packet_h, packet_w, packet_h);
+            if let Some(msg) = msg {
+                s.socket.send_reliably_to(msg, src, None)?;
             }
         }
-
-        Ok(())
     }
 
-    /// Create message ready for sending
-    fn wrap_game_rect(&self, x: usize, y: usize, w: usize, h: usize) -> Option<Message> {
-        let (pixels, w, h) = self.game.get_tilenet_serial_rect(x, y, w, h);
-        if w * h == 0 {
-            warn!("zero-size chunk of the world requested");
-            None
-        } else {
-            Some(Message::WorldRect {
-                x,
-                y,
-                width: w,
-                pixels,
-            })
-        }
-    }
+    Ok(())
+}
 
-    /// ASSUMPTION: packet size is 2^n
-    fn packet_dim(mut packet_size: usize) -> (usize, usize) {
-        packet_size -= 64; // Assume that the other fields take this amt of bytes...
-        let n = (packet_size as f32).log(2.0).floor();
-        (
-            2.0.powf((n / 2.0).ceil()) as usize,
-            2.0.powf((n / 2.0).floor()) as usize,
-        )
+/// Create message ready for sending
+fn wrap_game_rect(s: &Server, x: usize, y: usize, w: usize, h: usize) -> Option<Message> {
+    let (pixels, w, h) = s.game.get_tilenet_serial_rect(x, y, w, h);
+    if w * h == 0 {
+        warn!("zero-size chunk of the world requested");
+        None
+    } else {
+        Some(Message::WorldRect {
+            x,
+            y,
+            width: w,
+            pixels,
+        })
     }
+}
+
+/// ASSUMPTION: packet size is 2^n
+fn packet_dim(mut packet_size: usize) -> (usize, usize) {
+    packet_size -= 64; // Assume that the other fields take this amt of bytes...
+    let n = (packet_size as f32).log(2.0).floor();
+    (
+        2.0.powf((n / 2.0).ceil()) as usize,
+        2.0.powf((n / 2.0).floor()) as usize,
+    )
 }
 
 pub fn run(s: &mut Server) -> Result<(), Error> {
@@ -194,7 +192,7 @@ pub fn run(s: &mut Server) -> Result<(), Error> {
             messages.push(msg);
         }
         for msg in messages {
-            s.handle_message(msg.0, &msg.1)?;
+            handle_message(s, msg.0, &msg.1)?;
         }
 
         // Check ACKs

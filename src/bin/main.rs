6491481944c64bb7f219e02b7319e2bd50df40fe
Kevin Robert Stravers <macocio@gmail.com>
2019-01-22 18:25:02 +0100

    Remove double indentation and double {}

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -25,27 +25,25 @@ fn parse_command_line_arguments<'a>(s: &mut clap::ArgMatches<'a>) {
 fn run_client_or_server(mut s: glocals::Main) -> glocals::Main {
     let commandline = s.commandline.clone();
     if let Some(_connect) = commandline.value_of("connect") {
-        {
-            let (logger, thread) = crate::libs::logger::Logger::spawn();
-            s.threads.logger = Some(thread);
-            let mut client = Client {
-                logger,
-                should_exit: false,
-                main: s,
-                game: Game::default(),
-                display: glutin::WindowBuilder::new()
-                    .with_dimensions(1024, 768)
-                    .with_title("Universe")
-                    .build_glium()
-                    .unwrap(),
-                input: libs::input::Input::default(),
-                audio: rodio::Sink::new(&rodio::default_output_device().unwrap()),
-                logic_benchmarker: Benchmarker::new(99),
-                drawing_benchmarker: Benchmarker::new(99),
-            };
-            mediators::client::entry_point_client(&mut client);
-            client.main
-        }
+        let (logger, thread) = crate::libs::logger::Logger::spawn();
+        s.threads.logger = Some(thread);
+        let mut client = Client {
+            logger,
+            should_exit: false,
+            main: s,
+            game: Game::default(),
+            display: glutin::WindowBuilder::new()
+                .with_dimensions(1024, 768)
+                .with_title("Universe")
+                .build_glium()
+                .unwrap(),
+            input: libs::input::Input::default(),
+            audio: rodio::Sink::new(&rodio::default_output_device().unwrap()),
+            logic_benchmarker: Benchmarker::new(99),
+            drawing_benchmarker: Benchmarker::new(99),
+        };
+        mediators::client::entry_point_client(&mut client);
+        client.main
     } else {
         s
     }

e6173c29dff37dd69e5b1478958e9b9bc8a1ef63
Kevin Robert Stravers <macocio@gmail.com>
2019-01-23 02:11:02 +0100

    Implement setting the log level using gsh
    We use the following:
    
    	echo -ne "log level 128" | netcat 127.0.0.1 32931
    
    Currently, this doesn't appear to work. For some reason, the value isn't
    parseable.

--- src/mediators/game_shell.rs
+++ src/mediators/game_shell.rs
@@ -1,6 +1,6 @@
 use crate::glocals::{GameShell, Log};
 use crate::libs::{logger::Logger, metac::{Data, Evaluate}};
-use std::io::Read;
+use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 use std::str::from_utf8;
 use std::thread::{self, JoinHandle};
@@ -20,7 +20,10 @@ fn game_shell_thread(mut s: GameShell) {
             let string = from_utf8(&buffer[..]);
             if let Ok(string) = string {
                 s.logger.info(Log::Dynamic(string.into()));
-                s.interpret(string);
+                let result = s.interpret(string);
+                if let Ok(result) = result {
+                    stream.write(result.as_bytes());
+                }
             } else {
                 s.logger.warn(Log::Static("Malformed data from client"));
             }
@@ -34,11 +37,12 @@ impl Evaluate<String> for GameShell {
     fn evaluate<'a>(&mut self, commands: &[Data<'a>]) -> String {
         match commands[0] {
             Data::Atom("log") => {
-                self.log(&commands[1..]);
+                self.log(&commands[1..])
             }
-            _ => {}
+            _ => {
+                "".into()
+            }
         }
-        "".into()
     }
 }
 
@@ -47,11 +51,42 @@ impl Evaluate<String> for GameShell {
 impl GameShell {
     fn log<'a>(&mut self, commands: &[Data<'a>]) -> String {
         match commands[0] {
+            Data::Atom("level") => {
+                if commands.len() == 2 {
+                    match commands[1] {
+                        Data::Atom(number) => {
+                            let value = number.parse::<u8>();
+                            if let Ok(value) = value {
+                                self.logger.set_log_level(value);
+                                "OK: Changed log level".into()
+                            } else {
+                                self.logger.info(Log::Dynamic(String::from("|") + number.into() + "|"));
+                                "Err: Unable to parse number".into()
+                            }
+                        }
+                        _ => {
+                            "Usage: log level <u8>".into()
+                        }
+                    }
+                } else {
+                    "Usage: log level <u8>".into()
+                }
+            }
             Data::Atom(string) => {
                 self.logger.info(Log::Dynamic(string.into()));
+                "Unknown atom".into()
             }
-            _ => {}
+            _ => {
+                "Unknown command".into()
+            }
         }
-        "".into()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn parse_u8() {
+        "10".parse::<u8>().unwrap();
     }
 }

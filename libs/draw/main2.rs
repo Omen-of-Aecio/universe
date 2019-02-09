30e74a8098e64b05a1442864e406d6bf2d7ef1cf
Kevin Robert Stravers <macocio@gmail.com>
2019-02-09 13:07:49 +0100

    draw: Upload the drawing library (WIP!)
    Still a work in progress.

---
+++ libs/draw/main2.rs
@@ -0,0 +1,52 @@
+use draw::Draw;
+use gfx_hal::window::Extent2D;
+use winit;
+#[cfg(feature = "dx12")]
+use gfx_backend_dx12 as back;
+#[cfg(feature = "gl")]
+use gfx_backend_gl as back;
+#[cfg(feature = "metal")]
+use gfx_backend_metal as back;
+#[cfg(feature = "vulkan")]
+use gfx_backend_vulkan as back;
+use gfx_hal::format::{AsFormat, ChannelType, Rgba8Srgb as ColorFormat, Swizzle};
+use gfx_hal::Instance;
+
+const DIMS: Extent2D = Extent2D { width: 1024, height: 768 };
+
+fn main() {
+    let mut events_loop = winit::EventsLoop::new();
+
+    let wb = winit::WindowBuilder::new()
+        .with_dimensions(winit::dpi::LogicalSize::new(
+            DIMS.width as _,
+            DIMS.height as _,
+        ))
+        .with_title("quad".to_string());
+    // instantiate backend
+    #[cfg(not(feature = "gl"))]
+    let (_window, _instance, mut adapters, mut surface) = {
+        let window = wb.build(&events_loop).unwrap();
+        let instance = back::Instance::create("gfx-rs quad", 1);
+        let surface = instance.create_surface(&window);
+        let adapters = instance.enumerate_adapters();
+        (window, instance, adapters, surface)
+    };
+    #[cfg(feature = "gl")]
+    let (mut adapters, mut surface) = {
+        let window = {
+            let builder =
+                back::config_context(back::glutin::ContextBuilder::new(), ColorFormat::SELF, None)
+                    .with_vsync(true);
+            back::glutin::GlWindow::new(wb, builder, &events_loop).unwrap()
+        };
+
+        let surface = back::Surface::from_window(window);
+        let adapters = surface.enumerate_adapters();
+        (adapters, surface)
+    };
+
+    let mut draw = Draw::new(&mut surface);
+
+    println!["{:?}", draw.acquire_swapchain_image()];
+}

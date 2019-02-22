4ba02689cfb429c6c5875571c782ff3dac3840f8
Kevin Robert Stravers <macocio@gmail.com>
2019-02-22 18:57:16 +0100

    draw: Create an independent triangle struct!

--- libs/draw/main2.rs
+++ libs/draw/main2.rs
@@ -50,16 +50,18 @@ fn main() {
     };
 
     let mut draw = Draw::new(&mut surface);
+    let tri = draw.create_static_white_2d_triangle(&[-0.5, 0.5, -0.5, -0.5, 0.0, 0.0]);
     loop {
         for i in 0..100 {
             let image = draw.acquire_swapchain_image();
             // println!["{:?}", image];
             if let Some(image) = image {
                 draw.clear(image, i as f32 / 100f32 );
-                let triangle = Triangle {
-                    points: [[-0.5, (i as f32 / 100.0f32)], [-0.5, -0.5], [0.0, 0.0]],
-                };
-                draw.draw_triangle(image, triangle);
+                // let triangle = Triangle {
+                //     points: [[-0.5, (i as f32 / 100.0f32)], [-0.5, -0.5], [0.0, 0.0]],
+                // };
+                // draw.draw_triangle(image, triangle);
+                tri.draw(&mut draw, image);
                 // draw.render(image);
                 draw.swap_it(image);
             }

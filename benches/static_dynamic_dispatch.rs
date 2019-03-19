96c9e3d2e99c99caef0ccb3a1cb2d7c3a15f437a
Kevin R. Stravers <macocio@gmail.com>
2019-03-19 02:59:16 +0100

    benches: Add static/dynamic dispatch cost analysis
    Here are the results on my machine:
    
    test dynamic_dispatch ... bench:     178,973 ns/iter (+/- 11,280)
    test static_dispatch  ... bench:      60,561 ns/iter (+/- 4,047)
    
    Not surprising, the dynamic version runs about 3x slower.
    In both cases we see the overhead being around 0.5-1.5 nanoseconds,
    which is quite OK.
    
    Of course, running with static dispatch will be more friendly to the
    optimizer and the prefetcher.

---
+++ benches/static_dynamic_dispatch.rs
@@ -0,0 +1,38 @@
+//! Compare static and dynamic dispatch on an object
+#![feature(test)]
+extern crate test;
+
+use std::sync::mpsc;
+use test::{black_box, Bencher};
+
+trait T {
+    fn call(&self) -> Option<()>;
+}
+
+struct C {}
+impl T for C {
+    fn call(&self) -> Option<()> { Some(()) }
+}
+
+fn static_call(obj: &C) -> Option<()> { obj.call() }
+fn dynamic_call(obj: &T) -> Option<()> { obj.call() }
+
+#[bench]
+fn static_dispatch(b: &mut Bencher) {
+    let c = C {};
+    b.iter(|| {
+        for _ in 0..100_000 {
+            black_box(static_call(black_box(&c))).unwrap();
+        }
+    });
+}
+
+#[bench]
+fn dynamic_dispatch(b: &mut Bencher) {
+    let c = C {};
+    b.iter(|| {
+        for _ in 0..100_000 {
+            black_box(dynamic_call(black_box(&c))).unwrap();
+        }
+    });
+}

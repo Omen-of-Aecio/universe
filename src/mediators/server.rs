d95af8b5064a2a3bf873ebac0965c9526eb97a0f
Kevin R. Stravers <macocio@gmail.com>
2019-03-30 23:41:52 +0100

    server: Start with a simple loop

--- src/mediators/server.rs
+++ src/mediators/server.rs
@@ -1,3 +1,23 @@
 use crate::glocals::*;
 
-pub fn entry_point_server(_s: &mut Main) {}
+pub fn entry_point_server(s: &mut Main) {
+    loop {
+        s.timers.time = std::time::Instant::now();
+        server_tick(s);
+        if s.server.is_none() {
+            break;
+        }
+    }
+}
+
+fn server_tick(_s: &mut Main) {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn exit_server_if_none() {
+        let mut main = Main::default();
+        entry_point_server(&mut main);
+    }
+}

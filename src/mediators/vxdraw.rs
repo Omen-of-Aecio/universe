fc18b674a3583f2dc36ddae12e885bb810cd8581
Kevin Robert Stravers <macocio@gmail.com>
2019-04-06 20:30:01 +0200

    vxdraw: Don't run tests unless gfx_tests has been specified

--- src/mediators/vxdraw.rs
+++ src/mediators/vxdraw.rs
@@ -8,7 +8,7 @@ use gfx_backend_metal as back;
 #[cfg(feature = "vulkan")]
 use gfx_backend_vulkan as back;
 // use gfx_hal::format::{AsFormat, ChannelType, Rgba8Srgb as ColorFormat, Swizzle};
-use gfx_hal::{Backend, Instance};
+use gfx_hal::{window::Surface, Backend, Instance};
 use logger::{info, InDebug, Logger};
 use winit::{Event, EventsLoop, Window};
 
@@ -20,13 +20,19 @@ pub fn init_window_with_vulkan(log: &mut Logger<Log>) -> Windowing {
     let version = 1;
     let vk_inst = back::Instance::create("renderer", version);
     let surf: <back::Backend as Backend>::Surface = vk_inst.create_surface(&window);
-    let adapters = vk_inst.enumerate_adapters();
+    let mut adapters = vk_inst.enumerate_adapters();
     let len = adapters.len();
     info![log, "vxdraw", "Adapters found"; "count" => len];
     for (idx, adap) in adapters.iter().enumerate() {
         let info = adap.info.clone();
         info![log, "vxdraw", "Adapter found"; "idx" => idx, "info" => InDebug(&info)];
     }
+    // TODO Find appropriate adapter, I've never seen a case where we have 2+ adapters, that time
+    // will come one day
+    let adapter = adapters.remove(0);
+    adapter
+        .open_with::<_, gfx_hal::Graphics>(1, |family| surf.supports_queue_family(family))
+        .expect("Unable to find device supporting graphics");
     Windowing {
         surf,
         vk_inst,
@@ -49,6 +55,7 @@ pub fn collect_input(windowing: &mut Windowing) -> Vec<Event> {
 mod tests {
     use super::*;
 
+    #[cfg(feature = "gfx_tests")]
     #[test]
     fn init_window_and_get_input() {
         let mut logger = Logger::spawn();

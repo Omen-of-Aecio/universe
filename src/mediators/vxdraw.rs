68e8b49ee255353403056d5fa3b2db7d0380a615
Kevin Robert Stravers <macocio@gmail.com>
2019-04-19 12:07:16 +0200

    vxdraw: Test that strtex respects depth buffering

--- src/mediators/vxdraw.rs
+++ src/mediators/vxdraw.rs
@@ -2039,6 +2039,24 @@ mod tests {
         assert_swapchain_eq(&mut windowing, "three_layer_scene", img);
     }
 
+    #[test]
+    fn streaming_texture_respects_z_ordering() {
+        let mut logger = Logger::spawn_void();
+        let mut windowing = init_window_with_vulkan(&mut logger, ShowWindow::Headless1k);
+        let prspect = gen_perspective(&mut windowing);
+
+        let strtex1 = add_streaming_texture(&mut windowing, 10, 10, &mut logger);
+        strtex::streaming_texture_set_pixels_block(&mut windowing, strtex1, (0, 0), (9, 9), (255, 255, 0, 255));
+        strtex::streaming_texture_add_sprite(&mut windowing, strtex::Sprite::default(), strtex1);
+
+        let strtex2 = add_streaming_texture(&mut windowing, 10, 10, &mut logger);
+        strtex::streaming_texture_set_pixels_block(&mut windowing, strtex2, (1, 1), (9, 9), (0, 255, 255, 255));
+        strtex::streaming_texture_add_sprite(&mut windowing, strtex::Sprite { depth: 0.1,..strtex::Sprite::default() }, strtex2);
+
+        let img = draw_frame_copy_framebuffer(&mut windowing, &mut logger, &prspect);
+        assert_swapchain_eq(&mut windowing, "streaming_texture_z_ordering", img);
+    }
+
     // ---
 
     #[bench]

bdc72fa0b1a057ec5cadedecab4bd434b1da14f7
Kevin Robert Stravers <macocio@gmail.com>
2019-04-29 20:59:11 +0200

    build: Fix setting the pre-commit script

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 fn main() {
     #[cfg(target_os = "linux")]
     {
-        match std::os::unix::fs::symlink(".pre-commit", ".git/hooks/pre-commit") {
+        match std::os::unix::fs::symlink("../../.pre-commit", ".git/hooks/pre-commit") {
             Ok(()) => {}
             Err(ioerr) => {
                 if ioerr.kind() != std::io::ErrorKind::AlreadyExists {

ae5ae79397b94d82b2de03e99f2f59380b2bed19
Kevin Robert Stravers <macocio@gmail.com>
2019-05-26 21:46:13 +0200

    vxdraw: Rename init_window_with_vulkan -> new

--- src/mediators/client.rs
+++ src/mediators/client.rs
@@ -199,7 +199,7 @@ fn toggle_camera_mode(s: &mut Logic) {
 }
 
 pub fn maybe_initialize_graphics(s: &mut Main) {
-    let mut windowing = init_window_with_vulkan(s.logger.clone().to_logpass(), ShowWindow::Enable);
+    let mut windowing = Windowing::new(s.logger.clone().to_logpass(), ShowWindow::Enable);
 
     {
         static BACKGROUND: &[u8] = include_bytes!["../../assets/images/terrabackground.png"];
